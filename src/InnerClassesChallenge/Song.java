5d6432acd10b463764c99c7df0bbbe683193e366
Glen Roberts <glensroberts@gmail.com>
2016-04-22 23:40:19 -0400

    Need to complete InnerClassesChallenge

---
+++ src/InnerClassesChallenge/Song.java
@@ -0,0 +1,23 @@
+package InnerClassesChallenge;
+
+/**
+ * Created by robertsg on 12/17/2015.
+ */
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
